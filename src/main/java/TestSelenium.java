d6a34465b150b65167070b11c8446c81849977c2
Cesar Sebastian Ayala Rodriguez <cesar.ayala@itti.digital>
2025-04-14 16:21:38 -0300

    Test modificado y prueba automatizada en github aplicada

--- src/main/java/TestSelenium.java
+++ src/main/java/TestSelenium.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,6 +20,8 @@ public class TestSelenium {
         System.out.println("Título: " + driver.getTitle());
         driver.manage().timeouts();
         driver.manage().window().maximize();
+        WebElement emailField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='email']")));
+
         driver.findElement(By.xpath("//input[@name='email']")).sendKeys(page.getMail());
         driver.findElement(By.xpath("//input[@name='password']")).sendKeys(page.getPassword());
         driver.findElement(By.xpath("//button[@class='MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeLarge MuiButton-containedSizeLarge MuiButton-colorPrimary MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-sizeLarge MuiButton-containedSizeLarge MuiButton-colorPrimary css-f4qyey']")).click();
